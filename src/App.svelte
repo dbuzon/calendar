797621380082340466fdd599766f4423275370e7
Debora Buzon <debora.buzon@gmail.com>
2022-07-29 11:33:27 -0300

    Create login and register components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+	import Login from './components/Login.svelte';
+    import Register from './components/Register.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header class="logo">
+        <h1>kawaii calendar</h1>
+    </header>
+
+    <Login />
+    <Register />
+    
+    <footer>Made with ♥ by <a href="github.com/dbuzon">@dbuzon</a> </footer>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		max-width: 1000px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
---
+++ src/components/Login.svelte
@@ -0,0 +1,52 @@
+<script>
+    let inputEmail = "";
+    let inputPass = "";
+
+    const login = async () => {
+        const res = await fetch('http://localhost:3000/api/users', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: inputEmail,
+                password: inputPass
+            }),
+        });
+        const data = await res.json();
+        console.log(data); 
+    }
+</script>
+
+<div class="card">
+    <h2 class="title">Welcome</h2>
+
+    <label for="email">Email</label>
+    <input id="email" type="email" bind:value={inputEmail} placeholder="email@example.com" required>
+    
+    <label for="password">Password</label>
+    <input id="password" type="password" bind:value={inputPass} placeholder="******" required>
+    
+    <div class="button" on:click={login}>Sing in</div>
+</div>
+
+<style>
+    .card {
+        max-width: 500px;
+        padding: 2.5em 1.5em;
+        margin: 20px;
+        background-color: #FFF6F8;
+        border: 1px solid #FFC2D1;
+        border-radius: 20px;
+    }    
+
+    .title {
+        text-align: center;
+        margin-top: 0;
+        color: #FB6F92;
+        font-weight: 800;
+        line-height: 1;
+    }
+
+</style>
